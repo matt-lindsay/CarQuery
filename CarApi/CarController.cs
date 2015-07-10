efd0ad0ab5fcb420d00da16f7b8acebec192ef09
matt-lindsay <matt_epilp@hotmail.com>
2015-07-10 09:42:21 +0100

    Second commit, Car controller added.

---
+++ CarApi/CarController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CarApi
+{
+    public class Car
+    {
+        public int ID { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+    }
+ 
+    public class CarController : ApiController
+    {
+        public IEnumerable<Car> Get()
+        {
+            return new List<Car>
+            {
+                new Car { ID = 0, Make = "Ford", Model = "Kuga" }
+                new Car { ID = 1, Make = "BMW", Model = "325i" }
+                new Car { ID = 2, Make = "Audi", Model = "A6" }
+            };
+        }
+    }
+}
